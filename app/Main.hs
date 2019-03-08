d804e44d83bec4e2c1e07d7065705e7438a299b0
Arthur Chaloin <arthur@achaloin.com>
2019-03-08 16:31:05 +0100

    feat(jit): properly handle EOF

--- app/Main.hs
+++ app/Main.hs
@@ -6,12 +6,20 @@ import           System.IO
 import           System.Environment
 import           System.Exit
 import           System.Command
+import           Control.Exception
 
 import qualified Syntax
 import qualified Compiler
 import qualified LLVM.Module
 
 import Debug.Trace
+
+
+import Data.String
+import Data.String.Encode
+import LLVM.AST.Type         as LLVM
+
+import Syntax
 
 getFilename :: IO (Maybe String)
 getFilename = do
@@ -20,22 +28,26 @@ getFilename = do
 
 runInterpreter :: [Syntax.Expr] -> IO ()
 runInterpreter mod = do
-  putStr "koak> "
+  putStr "> "
   hFlush stdout
-  input <- getLine
-  let finput = if last input /= ';' then input ++ ";" else input
-  case Syntax.parse finput of
-    Left ((i, j), err) -> putStrLn err
-    Right exprs -> do
-      p <- Compiler.hasArg "--ast"
-      case p of
-        True -> do
-          putStrLn "======= AST ======="
-          putStrLn (show exprs)
-          putStrLn "===================\n"
-        _ -> pure ()
-      Compiler.jit $ (previousFuncs mod) ++ exprs
-      runInterpreter $ (previousFuncs mod) ++ exprs
+  eof <- isEOF
+  if eof then pure () else do
+    input <- getLine
+    let finput = if last input /= ';' then input ++ ";" else input
+    case Syntax.parse finput of
+      Left ((i, j), err) -> putStrLn err
+      Right exprs -> do
+        p <- Compiler.hasArg "--ast"
+        case p of
+          True -> do
+            putStrLn "======= AST ======="
+            putStrLn (show exprs)
+            putStrLn "===================\n"
+          _ -> pure ()
+        result <- try (Compiler.jit $ (previousFuncs mod) ++ exprs) :: IO (Either SomeException ())
+        case result of
+          Left e -> undefined
+          _ -> runInterpreter $ (previousFuncs mod) ++ exprs
   where
     previousFuncs b = [fn f | f <- b, isPersistent f]
     isPersistent (Syntax.Function {}) = True
